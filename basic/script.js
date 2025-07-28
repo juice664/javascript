c802fd17d94c6ee9650b1c7f3347678cd149ea8e
sagar <sagarkhatri17t@gmail.com>
2025-07-28 11:40:12 +0545

    FIRST COMMIT

---
+++ basic/script.js
@@ -0,0 +1,172 @@
+// console.log("Pramod");
+// // alert("Hello this is me !")
+// // document.writeln("hello how are You")
+// // let x=prompt("Enter your name !")
+// // document.writeln(x)
+// // console.log(100+100)
+// // console.log(500-100)
+// // console.log(500*100)
+// // console.log(500/100)
+// // console.log(500%100)
+// // console.log(5**2)
+
+
+
+// // primitive Data types (7)
+
+// // String
+
+// // val="abcde"
+// // console.log(val)
+// // console.log(typeof val)
+
+
+// // Number
+// // x=12345
+// // console.log(typeof x)
+
+
+// // Boolean
+// // isBol=true //false
+// // console.log(isBol)
+// // console.log(typeof isBol)
+
+// // Undefined
+// // let var const
+// let x
+// console.log(x)
+
+// // Null
+
+// let y=null
+// console.log(y)
+//  console.log(typeof y) //data type of null is object
+
+// // Symbol
+// let id1=Symbol(12345)
+// let id2=Symbol(12345)
+
+// console.log(id1)
+
+// // Bigint
+
+// let m=1234567896565465446545465
+// let n=BigInt(123456708695)
+// console.log(n)
+// console.log(m)
+
+//rule
+// _,$, character, 
+
+//let vs var vs const
+
+//const
+//const.. value cannot be changed , same name variable not allowed , it cannot be empty.
+
+//let
+//update is allowed , same name variable not allowed ,it can be empty.
+
+//var
+//update is allowed, same name variable allowed , it can be empty.
+
+
+
+
+
+
+
+//non-primitive data types(3)
+//array= collection of data
+
+// let arr=[1,2,3,4,5,"hello",true,null,undefined]
+// console.log(typeof arr) 
+// console.log(arr[0])
+
+
+
+
+// //,object
+
+//     let user={
+//         name:"ram",
+//         age:25,
+//         phonenumber:"1234567890"
+//     }
+//     console.log(typeof user)
+//     console.log(user)
+//     console.log(user.age)
+
+//  function
+//  function= block of code
+// function add(a,b){
+//     console.log(a)
+//     console.log(b)
+//     console.log(a+b)}
+// add(10,20)
+
+// Arithmetic Operators
+// +,-,*,/,%,**,++,--
+
+// let a =10
+// let b =20
+// console.log(a+b) // addition
+// console.log(a-b) // subtraction 
+// console.log(a*b) // multiplication
+// console.log(a/b) // division
+// console.log(a%b) // modulus/reminder
+// console.log(a**b) // exponentiation/power
+// console.log(a++)
+// console.log(a) // post-increment
+// console.log(++a) // pre-increment
+
+
+// Assignment Operators
+
+// =-,=+,=*,=/,%=,**=
+
+// let x=5
+// x+=5 // x = x + 5
+// console.log(x)
+// x-=5 // x = x - 5
+// console.log(x)
+// x*=5 // x = x * 5
+// console.log(x)          
+// x/=5 // x = x / 5
+// console.log(x)
+// x%=5 // x = x % 5
+// console.log(x)
+// x**=5 // x = x ** 5
+// console.log(x) // exponentiation assignment
+
+
+
+// Comparison Operators
+// ==,===,!=,!==,>,<,>=,<=
+ let a=10
+ let b=5
+ console.log(a==b) // loose equality
+ console.log(a===b) // strict equality
+ console.log(a!=b) // loose inequality
+ console.log(a!==b) // strict inequality
+
+
+
+
+// Logical Operators
+// &&,||,!
+
+// ternary operator
+// condition ? true : false
+
+
+// variable name rules P
+// orators 
+// conditional statement 
+// loop 
+// switch 
+// array  (methods)
+// object (methods)
+// string (Methods)
+// function 
+// class and object 
+// dom
