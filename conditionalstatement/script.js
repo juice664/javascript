542c4f1f24a200e9ebce7d434918cb9c65de7490
sagar <sagarkhatri17t@gmail.com>
2025-07-28 11:43:00 +0545

    second commit

---
+++ conditionalstatement/script.js
@@ -0,0 +1,196 @@
+// console.log("This Conditional statement")
+
+// const { jsx } = require("react/jsx-runtime")
+
+// if(false){
+//     console.log("true")
+// }
+// else if(false){
+//     console.log("else if")
+// }
+
+// else{
+//     console.log("false")
+// }
+
+ //let age = 12
+
+ //if (age<=0)[
+ //    console.log("Not Born")
+// ]
+//else if (age<13){
+  //   console.log("Child")
+ //}
+ //else if(age<20){
+  //   console.log("Teen")
+//}
+// else if(age<40){
+   //  console.log("Yong")
+ //}
+ //else{
+   // console.log("Old")
+ //}
+
+// Grade system
+// 0-29 > fail
+// 30 above > D
+// 40 above < C
+// 50 above < c+
+// 60 above < B
+// 70 above < B+
+// 80 above < A
+// 90-100 Above <A+
+
+//let score = 50
+
+ //if (score<=30){
+ //   console.log("Fail")
+ //}
+ //else if(score<=40){
+ //    console.log("D")
+ //}
+ //else if(score<=50){
+  //   console.log("C")
+ //}
+//else if(score<=60){
+  //  console.log("C+")
+ //}
+ //else if(score<=70){
+  //   console.log("B")
+ //}
+ //else if(score<=80){
+  //   console.log("B+")
+// }
+ //else if(score<=90){
+ //    console.log("A")
+ //}
+ //else if(score<=100){
+ //    console.log("A+")
+ //}
+
+
+// let n = 100
+// if(n%2==0){
+//     console.log("Even Number")
+// }
+// else{
+//     console.log("Odd number")
+// }
+
+// ----------------------------------
+//switch
+
+// let type = "a";
+
+// switch(type){
+//     case "a":{
+//         console.log("a");
+//         break;
+//     }
+//     case "b":{
+//         console.log("B");
+//         break;
+//     }
+//     case "c":{
+//         console.log("c");
+//         break;
+//     }
+//     default:{
+//         console.log("default")
+//     }
+// }
+
+// let age = 2
+// switch(true){
+//     case (age<=0):{
+//         console.log("Not born");
+//         break;
+//     }
+//     case age<=13:{
+//         console.log("Child");
+//         break;
+//     }
+//     case age<=20:{
+//         console.log("teen");
+//         break;
+//     }
+//     case age<=40:{
+//         console.log("Yong");
+//         break;
+//     }
+//     default:{
+//         console.log("Old")
+//     }
+// }
+
+// let score = 100
+
+// switch (true){
+//     case score>0 && score<=29:{
+//         console.log("Fail");
+//         break;
+//     }
+//     case score>=39:{
+//         console.log("D");
+//         break
+//     }
+//     case score>=49:{
+//         console.log("C");
+//         break;
+//     }
+//     case score>=59:{
+//         console.log("C+");
+//         break;
+//     }
+//     case score>=69:{
+//         console.log("B");
+//         break;
+//     }
+//     case score>=79:{
+//         console.log("B+");
+//         break;
+//     }
+//     case score>=89:{
+//         console.log("A");
+//         break;
+//     }
+//     case score>=90 && score>=100:{
+//         console.log("A+");
+//         break;
+//     }
+
+// }
+// -----------------------------===--------------
+
+// Loop in js
+// 1, for Loop
+// 2, while
+// 3, do while
+
+// for loop
+
+// for(let i=0;i<=10;i++){
+//     // console.log("Hello Ram");
+//     console.log(i)
+// }
+// for(let i=10; i<=20;i++){
+//     console.log(i)
+// }
+// for(let i=100; i<=200;i++){
+//     console.log(i)
+// }
+
+// for(let i=20; i>=1;i--){
+//     console.log(i)
+// }
+
+// for(let i=1;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+//for(let i=1;i<=20;i++){
+  //  if(i%2!==0){
+   //     console.log(i)
+   // }
